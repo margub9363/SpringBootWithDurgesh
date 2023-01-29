354bbdce93805c34149129208952966caad0a7dd
rahman <margub.9363@gmail.com>
2023-01-30 00:47:31 +0530

    Initial Project Setup with Error

---
+++ src/main/java/com/example/springbootwithdurgesh/SpringBootWithDurgeshApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithdurgesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithDurgeshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithDurgeshApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootwithdurgesh/SpringBootWithDurgeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithdurgesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithDurgeshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
