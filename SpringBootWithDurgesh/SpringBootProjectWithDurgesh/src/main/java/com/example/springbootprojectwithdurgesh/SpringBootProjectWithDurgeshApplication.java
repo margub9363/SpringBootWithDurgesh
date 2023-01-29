7765c08e621c76ff9a55d499821836ecde791a6a
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-01-30 00:54:19 +0530

    Add files via upload

---
+++ SpringBootWithDurgesh/SpringBootProjectWithDurgesh/src/main/java/com/example/springbootprojectwithdurgesh/SpringBootProjectWithDurgeshApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootprojectwithdurgesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectWithDurgeshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProjectWithDurgeshApplication.class, args);
+    }
+
+}
---
+++ SpringBootWithDurgesh/SpringBootProjectWithDurgesh/src/test/java/com/example/springbootprojectwithdurgesh/SpringBootProjectWithDurgeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootprojectwithdurgesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectWithDurgeshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
