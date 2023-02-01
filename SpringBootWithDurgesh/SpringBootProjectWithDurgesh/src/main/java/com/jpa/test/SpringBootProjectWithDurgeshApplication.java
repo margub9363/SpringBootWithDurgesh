8bbacd4836c5eaa820bf06a768a9028d719314ed
rahman <margub.9363@gmail.com>
2023-02-01 23:06:55 +0530

    10-12

--- SpringBootWithDurgesh/SpringBootProjectWithDurgesh/src/main/java/com/jpa/test/SpringBootProjectWithDurgeshApplication.java
+++ SpringBootWithDurgesh/SpringBootProjectWithDurgesh/src/main/java/com/jpa/test/SpringBootProjectWithDurgeshApplication.java
@@ -1,13 +1,25 @@
 package com.jpa.test;
 
+import com.jpa.test.dao.UserRepository;
+import com.jpa.test.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootProjectWithDurgeshApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootProjectWithDurgeshApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringBootProjectWithDurgeshApplication.class, args);
+
+        UserRepository userRepository =context.getBean(UserRepository.class);
+        User user = new User();
+        user.setName("Rahman");
+        user.setCity("Ranchi");
+        user.setStatus("Full Stack Developer");
+
+        User user1 = userRepository.save(user);
+        System.out.println(user1);
     }
 
 }
